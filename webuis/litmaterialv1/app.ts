6ef2a6ce557d9812db4ce44d477ec2ae7cc38028
Mohamed Mansour <hello@mohamedmansour.com>
2023-08-09 16:17:43 -0700

    Add Lit Material

---
+++ webuis/litmaterialv1/app.ts
@@ -0,0 +1,14 @@
+import {LitElement, html} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+import '@material/web/button/filled-button.js';
+
+@customElement('example-app')
+export class ExampleApp extends LitElement {
+  render() {
+    return html`
+    <div>
+        <md-filled-button>Material Button</md-filled-button>
+    </div>`
+  }
+}

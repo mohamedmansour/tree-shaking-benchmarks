9c771a31f528a6412b62f0d3740708ed73feab2b
Mohamed Mansour <hello@mohamedmansour.com>
2023-08-18 10:41:35 -0700

    Add SWC build

---
+++ swcpack.config.cjs
@@ -0,0 +1,15 @@
+const { config } = require("@swc/core/spack")
+
+module.exports = config({
+  entry: {
+    "lit-element": __dirname + '/webuis/lit-element/app.ts',
+    "lit-material": __dirname + '/webuis/lit-material/app.ts',
+    "fast-element": __dirname + '/webuis/fast-element/app.ts',
+    // "fast-fluent": __dirname + '/webuis/fast-fluent/app.ts',
+    // "react": __dirname + '/webuis/react/app.tsx',
+  },
+  output: {
+    path: __dirname + "/dist/swc",
+    name: "[name].js",
+  }
+})

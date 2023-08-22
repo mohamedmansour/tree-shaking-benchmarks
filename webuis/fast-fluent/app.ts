18416ecb0ef70e1701be47c2acece31d629bc2e4
Mohamed Mansour <mmansour@microsoft.com>
2023-08-22 10:58:16 -0700

    Update FluentUI to v3.0.0-alpha.31

--- webuis/fast-fluent/app.ts
+++ webuis/fast-fluent/app.ts
@@ -1,8 +1,8 @@
 import { customElement, html, FASTElement } from '@microsoft/fast-element-v3';
-import { DesignToken } from '@microsoft/fast-foundation-v3';
+import { DesignToken } from '@microsoft/fast-foundation-v3/design-token.js';
 import { setTheme } from '@fluentui/web-components-v3';
 import { webDarkTheme } from '@fluentui/tokens';
-import '@fluentui/web-components-v3/button'
+import '@fluentui/web-components-v3/button.js'
 
 @customElement({
   name: 'example-app',

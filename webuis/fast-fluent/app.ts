0324961cdf24dcc5e40cb8d3dd812b5e07da6471
Mohamed Mansour <mmansour@microsoft.com>
2023-08-22 13:07:38 -0700

    Rewrite all FluentUI imports as ESModule imports using esbuild plugin

--- webuis/fast-fluent/app.ts
+++ webuis/fast-fluent/app.ts
@@ -1,18 +1,17 @@
-import { customElement, html, FASTElement } from '@microsoft/fast-element-v3';
+import { css, customElement, html, FASTElement } from '@microsoft/fast-element-v3';
 import { DesignToken } from '@microsoft/fast-foundation-v3/design-token.js';
 import { setTheme } from '@fluentui/web-components-v3';
-import { webDarkTheme } from '@fluentui/tokens';
-import '@fluentui/web-components-v3/button.js'
+import { webLightTheme } from '@fluentui/tokens';
+import "@fluentui/web-components-v3/button.js";
 
 @customElement({
   name: 'example-app',
   template: html`
   <div>
     <fluent-button>Fluent Button</fluent-button>
-  </div>`,
+  </div>`
 })
 export class ExampleApp extends FASTElement {}
 
 DesignToken.registerDefaultStyleTarget();
-setTheme(webDarkTheme);
-
+setTheme(webLightTheme);

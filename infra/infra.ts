548d7741c6eb39d0b2b1bb2a536dae386fe97dcf
Mohamed Mansour <hello@mohamedmansour.com>
2023-09-19 10:11:46 -0700

    infra: remove bun

--- infra/infra.ts
+++ infra/infra.ts
@@ -1,7 +1,6 @@
 import EsbuildServeAction from './actions/esbuild_serve_action.js';
 import EsbuildBuildAction from './actions/esbuild_build_action.js';
 import WebpackBuildAction from './actions/webpack_build_action.js';
-import BunBuildAction from './actions/bun_build_action.js';
 
 import { Command } from 'commander';
 
@@ -14,6 +13,5 @@ program
 program.command('esbuild:serve').action(async () => await EsbuildServeAction());
 program.command('esbuild:build').action(async () => await EsbuildBuildAction());
 program.command('webpack:build').action(async () => await WebpackBuildAction());
-program.command('bun:build').action(async () => await BunBuildAction());
 
 program.parse();

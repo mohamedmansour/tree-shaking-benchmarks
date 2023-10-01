fc3c5d1fdb7605746d2b69eeca4e2dbe9747959e
Mohamed Mansour <hello@mohamedmansour.com>
2023-10-01 00:58:11 -0700

    esbuild: cleanup proxy

--- infra/actions/esbuild_serve_action.ts
+++ infra/actions/esbuild_serve_action.ts
@@ -1,7 +1,3 @@
-import fs from 'node:fs'
-import http from 'node:http'
-import { URL } from 'node:url'
-
 import * as esbuild from 'esbuild'
 
 import { EsbuildBaseAction } from './esbuild_base_action.js'
@@ -9,13 +5,6 @@ import { copyFolder } from '../utils/file_utils.js'
 import { ActionOptions } from './base_action.js'
 
 class EsbuildServeAction extends EsbuildBaseAction {
-  handlerMap: Map<string, (req: http.IncomingMessage, res: http.ServerResponse) => void> = new Map()
-
-  constructor(options: ActionOptions) {
-    super(options)
-    this.handlerMap.set('/api', (req, res) => this.handleApi(req, res))
-  }
-
   async run(): Promise<void> {
     // Build the app.
     const esbuildOptions: esbuild.BuildOptions = {
@@ -35,73 +24,11 @@ class EsbuildServeAction extends EsbuildBaseAction {
       onRequest: (args) => this.onEsbuildRequest(args)
     })
 
-    // Then start a proxy server on port 3000.
-    http.createServer((req, res) => this.onServerCreated(host, port, req, res)).listen(3000)
-
-    console.log(`[  ready] http://localhost:3000/`)
-  }
-
-  private onServerCreated(host: string,
-    port: number,
-    req: http.IncomingMessage,
-    res: http.ServerResponse) {
-    const options = {
-      hostname: host,
-      port: port,
-      path: req.url,
-      method: req.method,
-      headers: req.headers,
-    }
-
-    if (req.url) {
-      const reqUrl = new URL(req.url, "http://localhost").pathname
-      const customUrlHandler = this.handlerMap.get(reqUrl)
-      if (customUrlHandler) {
-        console.log(`[ custom] ${reqUrl}`)
-        customUrlHandler(req, res)
-        return
-      }
-    }
-
-    // Forward each incoming request to esbuild
-    const proxyReq = http.request(options, proxyRes => this.onEsbuildResponse(res, proxyRes))
-
-    // Forward the body of the request to esbuild
-    req.pipe(proxyReq, { end: true })
+    console.log(`[  ready] http://${host}:${port}/`)
   }
 
   private onEsbuildRequest(args: esbuild.ServeOnRequestArgs) {
     console.log(`[esbuild] ${args.path}`)
-  }
-
-  private onEsbuildResponse(res: http.ServerResponse, proxyRes: http.IncomingMessage) {
-    // If esbuild returns "not found", send a custom 404 page
-    if (proxyRes.statusCode === 404) {
-      this.handle404(res)
-      return
-    }
-
-    // Otherwise, forward the response from esbuild to the client
-    res.writeHead(proxyRes.statusCode || 500, proxyRes.headers)
-
-    proxyRes.pipe(res, { end: true })
-  }
-
-  private handle404(res: http.ServerResponse) {
-    res.writeHead(404, { 'Content-Type': 'text/html' })
-    res.end('<h1>404</h1>')
-  }
-
-  private handleApi(req: http.IncomingMessage, res: http.ServerResponse) {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.write(JSON.stringify({ status: 'ok' }))
-    res.end()
-  }
-
-  private deleteFolder(path: string): void {
-    if (fs.existsSync(path)) {
-      fs.rmSync(path, { recursive: true })
-    }
   }
 }
 

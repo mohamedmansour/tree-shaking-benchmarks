bbbe98a23f2274f5f64108e12dec7f87551f3c78
Mohamed Mansour <hello@mohamedmansour.com>
2023-09-30 19:23:42 -0700

    Revert "fix: partially disable fluent-hydration"
    This reverts commit 371ea9f40e76add9ad894a9da2f54a6a67160d22.

--- infra/config.ts
+++ infra/config.ts
@@ -9,8 +9,7 @@ export const ENTRY_POINTS: Record<string, string> = {
   'fast-fluent': './webuis/fast-fluent/app.ts',
   'react': './webuis/react/app.tsx',
   'react-fluent': './webuis/react-fluent/app.tsx',
-  // TODO: need fixing.
-  // 'react-fluent-hydration': './webuis/react-fluent-hydration/app.tsx',
+  'react-fluent-hydration': './webuis/react-fluent-hydration/app.tsx',
 }
 
 
